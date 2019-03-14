06aab1a8f8bf768a7a6fdfbfed43974a0b9cb4d9
林謙慈 <amylin19990210@gmail.com>
2019-03-14 16:38:20 +0800

    打掉重練備份檔

---
+++ index.js
@@ -0,0 +1,8 @@
+let buttonY = document.getElementById("button__start--yellow");
+let buttonB = document.getElementById("button__start--blue");
+
+console.log(buttonB);
+
+buttonB.addEventListener("click", function() {
+    console.log("test");
+});
